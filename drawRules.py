85f8be7d72b4744eb3998c562e745c658284404d
82Daniela <danielandraderock@gmail.com>
2019-04-10 21:01:19 +0000

    aaa

--- drawRules.py
+++ drawRules.py
@@ -6,7 +6,7 @@ ls = LabelSettings(LabelType.Term, LabelRelation.Unification)
 ######################################
 # RULES
 ######################################
-pushFilePrefix("../rules/")
+pushFilePrefix("rules/")
 opp_loss_gpp = ruleGML('opp_loss_gpp.gml')
 opp_loss_gpp = ruleGML('opp_loss_gpp_alternative.gml')
 h_loss = ruleGML("h_loss.gml")
